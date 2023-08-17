574f9f125a0ec8083297801f101571a4856e7fea
Jonathan Maia <jonathancmaia@gmail.com>
2023-08-16 23:47:49 -0300

    added whatsapp to links

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,21 +1,27 @@
-import './Intro.css'
-import logos from '../../assets/logo.png'
+import "./Intro.css";
+import logos from "../../assets/logo.png";
 
 const Intro = () => {
   return (
     <div className="intro-container">
-      <div className="intro" >
+      <div className="intro">
         <img src={logos} alt="Logo Komabem" data-aos="zoom-in-right" />
         <h4 data-aos="zoom-in-right">
           O Sabor da Tradição em cada prato! Conheça nossos congelados e a là
           cante que oferecemos diariamente.
         </h4>
         <button className="button" data-aos="zoom-in-right">
-        <a href="#">Faça seu Pedido</a>
+          <a
+            href="https://wa.me/+55021972770807"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Faça seu Pedido
+          </a>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
